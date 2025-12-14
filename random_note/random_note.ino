7897967965e3d475d156cfd8e82d7024a203ccfe
ledlaux <60392231+ledlaux@users.noreply.github.com>
2025-12-14 13:22:19 +0200

    Update random_note.ino

--- random_note/random_note.ino
+++ random_note/random_note.ino
@@ -32,33 +32,33 @@ extern "C" void __atomic_clear(volatile void* ptr, int memorder) {
 }
 #endif
 
-// --- Hardware random seed ---
-class Trand {
+// // --- Hardware random seed ---
+// class Trand {
 
-  public: Trand();
+//   public: Trand();
 
-};
+// };
 
-Trand::Trand() {
+// */Trand::Trand() {
   
-  uint32_t random = 0;
-  uint32_t random_bit;
-  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+//   uint32_t random = 0;
+//   uint32_t random_bit;
+//   volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
 
-  for (int k = 0; k < 32; k++) {
-    while (1) {
-      random_bit = (*rnd_reg) & 1;
-      if (random_bit != ((*rnd_reg) & 1)) break;
-    }
+//   for (int k = 0; k < 32; k++) {
+//     while (1) {
+//       random_bit = (*rnd_reg) & 1;
+//       if (random_bit != ((*rnd_reg) & 1)) break;
+//     }
 
-    random = (random << 1) | random_bit;
-  }
+//     random = (random << 1) | random_bit;
+//   }
   
-  srand(random);
+//   srand(random);
 
-}
+// }
 
-  Trand Trand;
+//   Trand Trand;
 
 
 // --- I2S config ---
