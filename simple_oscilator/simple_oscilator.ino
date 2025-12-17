cb4f364d7b02314b1ccf11b953f63e673a2af773
Vadims Maksimovs <ledlaux@gmail.com>
2025-12-18 01:14:47 +0200

    Update simple_oscilator.ino

--- simple_oscilator/simple_oscilator.ino
+++ simple_oscilator/simple_oscilator.ino
@@ -34,40 +34,12 @@ extern "C" void __atomic_clear(volatile void* ptr, int memorder) {
 #define I2S_WORDS     64
 #define SAMPLE_RATE   44100
 
-class Trand {
-
-  public: Trand();
-
-};
-
-Trand::Trand() {
-  
-  uint32_t random = 0;
-  uint32_t random_bit;
-  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
-
-  for (int k = 0; k < 32; k++) {
-    while (1) {
-      random_bit = (*rnd_reg) & 1;
-      if (random_bit != ((*rnd_reg) & 1)) break;
-    }
-
-    random = (random << 1) | random_bit;
-  }
-  
-  srand(random);
-
-}
-
-  Trand Trand;
   I2S i2s_output(OUTPUT);
   Heavy_oscilator pd_prog(SAMPLE_RATE);
 
   float samples[2 * I2S_WORDS];
   int16_t left_buffer[I2S_WORDS];
   int16_t right_buffer[I2S_WORDS];
-
-
 
 void setup() {
   i2s_output.setFrequency(SAMPLE_RATE);  // sets sample rate
@@ -85,7 +57,7 @@ void loop() {
   for (uint32_t i = 0; i < I2S_WORDS; i++) {
     
     left_buffer[i]  = int16_t(samples[2*i] * 32767.0f);
-right_buffer[i] = int16_t(samples[2*i+1] * 32767.0f);
+    right_buffer[i] = int16_t(samples[2*i+1] * 32767.0f);
     i2s_output.write16(right_buffer[i], left_buffer[i]);
 
   }
